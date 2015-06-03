80f213833e099cf73601a1b0f8e9059217d98859
Vive Vio Permana <vive.permana@gmail.com>
2015-06-03 07:40:22 +0700

    add change data algorithm

--- libs/helper.php
+++ libs/helper.php
@@ -1,1 +1,17 @@
 <?php
+
+function hasChange() {
+
+	$query = mysql_query("SELECT option_value FROM tb_option WHERE option_key='changes_data'");
+	$r = mysql_fetch_array( $query );
+	
+	return $r['option_value'];
+}
+
+function doChangesData( $value='1' )
+{
+	$query = mysql_query("UPDATE tb_option WHERE option_key='changes_data' SET option_value='$value'");
+	return $query;
+}
+
+?>
