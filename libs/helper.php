14f241120b6f5afbf3aaa4cb726e3bbd5a8d0e84
Vive Vio Permana <vive.permana@gmail.com>
2015-06-03 07:48:21 +0700

    add documentation

--- libs/helper.php
+++ libs/helper.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Melakukan pemeriksaan apakah telah terjadi perubahan data atau tidak.
+ * @return string  1 = jika terjadi perubahan data | 0 = tidak terjadi perubahan data
+ */
 function hasChange() {
 
 	$query = mysql_query("SELECT option_value FROM tb_option WHERE option_key='changes_data'");
@@ -8,8 +11,13 @@ function hasChange() {
 	return $r['option_value'];
 }
 
+/**
+ * Mengubah status changes_data pada table tb_option
+ * @param 	string 	$value    :  nilai status >> 1(default) = telah terjadi perubahan data | 0 = tidak terjadi perubahan data
+ */
 function doChangesData( $value='1' )
 {
+
 	$query = mysql_query("UPDATE tb_option WHERE option_key='changes_data' SET option_value='$value'");
 	return $query;
 }
