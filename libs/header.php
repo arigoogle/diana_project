2336ef255186169363c1c76632af962ff01c3f79
arigoogle <ariyantomail@gmail.com>
2015-06-05 22:44:02 +0700

    change modul header

---
+++ libs/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE htmlu>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Diana Project</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <link href="inspiritas.css" rel="stylesheet">
+</head>
+
+<body>
--- tambah_dosen.php
+++ tambah_dosen.php
@@ -15,24 +15,9 @@
     }
 ?>
 
-<!DOCTYPE htmlu>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Diana Project</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <link href="inspiritas.css" rel="stylesheet">
-</head>
-
-<body>
+<?php 
+  include'libs/header.php'; 
+?>
 
 <!-- Navbar
   ================================================== -->
