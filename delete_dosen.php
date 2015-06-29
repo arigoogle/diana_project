5b314da5673e557b91259db20bddd57d16181cda
Vive Vio Permana <vive.permana@gmail.com>
2015-06-29 16:50:28 +0700

    fix bugs when deleting dosen

--- delete_dosen.php
+++ delete_dosen.php
@@ -1,8 +1,9 @@
 <?php  
 	require_once 'libs/konek.php';
-	if (isset($_GET['id_dosen'])) 
+	if ( isset( $_GET['id_dosen'] ) ) 
 	{
 		mysql_query("DELETE FROM tb_dosen WHERE id_dosen = '$_GET[id_dosen]'");
+		mysql_query("DELETE FROM tb_ampu_dosen WHERE id_dosen = '$_GET[id_dosen]'");
 	}
 
 	header("location:dosen.php")
