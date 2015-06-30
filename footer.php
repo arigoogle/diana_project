de8b1ba60ceb0d6d1bde641b6053b44678d71c8a
Vive Vio Permana <vive.permana@gmail.com>
2015-06-30 09:01:53 +0700

    remove uneeded resource

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script src="js/highcharts.js"></script>
     <script src="js/inspiritas.js"></script>
     <script src="bootstrap/js/bootstrap-dropdown.js"></script>
     <script src="bootstrap/js/bootstrap-collapse.js"></script>
